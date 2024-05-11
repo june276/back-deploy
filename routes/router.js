da8a7ae98a7f8d359b7e6718150c32262d3d5649
june276 <jungjune276@gmail.com>
2024-05-11 16:23:40 +0900

    v2.2

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { main, test } from '../controller/controller';
+import { main, test } from '../controller/controller.js';
 
 const router = express.Router();
 
